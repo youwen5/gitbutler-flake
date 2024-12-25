e1eb6f4b1f115158f3428685d13c11cba55a17b4
Youwen Wu <youwenw@gmail.com>
2024-12-24 21:17:17 -0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Provides Gitbutler binary distributions for Nix.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+          inherit (pkgs) callPackage;
+        in
+        {
+          default = self.packages.${system}.gitbutler;
+          gitbutler = callPackage ./gitbutler.nix { };
+        }
+      );
+    };
+}
---
+++ gitbutler.nix
@@ -0,0 +1,56 @@
+{
+  stdenv,
+  dpkg,
+  autoPatchelfHook,
+  wrapGAppsHook3,
+  fetchurl,
+  webkitgtk_4_1,
+  libsoup_2_4,
+  lib,
+  git,
+  makeWrapper,
+  ...
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "gitbutler";
+  version = "0.14.4";
+  src = fetchurl {
+    url = "https://releases.gitbutler.com/releases/release/0.14.4-1569/linux/x86_64/GitButler_${finalAttrs.version}_amd64.deb";
+    hash = "sha256-z643LzoFxb5DKgNCe+h0naC1dA9/VfMAJ7ZSYhibhpQ=";
+  };
+
+  nativeBuildInputs = [
+    wrapGAppsHook3
+    autoPatchelfHook
+    dpkg
+    webkitgtk_4_1
+    libsoup_2_4
+    makeWrapper
+  ];
+
+  unpackPhase = "dpkg-deb -x $src unpack";
+
+  installPhase = ''
+    install -Dm755 unpack/usr/bin/gitbutler-tauri $out/bin/gitbutler-tauri
+    install -Dm755 unpack/usr/bin/gitbutler-git-setsid $out/bin/gitbutler-git-setsid
+    install -Dm755 unpack/usr/bin/gitbutler-git-askpass $out/bin/gitbutler-git-askpass
+
+    cp -r unpack/usr/share $out/share
+  '';
+
+  postFixup = ''
+    wrapProgram $out/bin/gitbutler-tauri \
+      --set PATH ${
+        lib.makeBinPath [
+          git
+        ]
+      }:$out/bin:$PATH
+  '';
+
+  meta = {
+    description = "Git client for simultaneous branches on top of your existing workflow";
+    license = lib.licenses.fsl11Mit;
+    mainProgram = "gitbutler-tauri";
+    platforms = [ "x86_64-linux" ];
+  };
+})
