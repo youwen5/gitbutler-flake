89d3807961d26bb202e4cdbb34e501c8f47a8850
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2024-12-26 16:10:22 -0500

    feat: add nvidia gpu wrappers

--- gitbutler.nix
+++ gitbutler.nix
@@ -41,6 +41,10 @@ stdenv.mkDerivation (finalAttrs: {
 
   postFixup = ''
     wrapProgram $out/bin/gitbutler-tauri \
+      --set __NV_PRIME_RENDER_OFFLOAD 1 \
+      --set __NV_PRIME_RENDER_OFFLOAD_PROVIDER "NVIDIA-G0" \
+      --set __GLX_VENDOR_LIBRARY_NAME "nvidia" \
+      --set __VK_LAYER_NV_optimus "NVIDIA_only" \
       --set PATH ${
         lib.makeBinPath [
           git
