f2868302353a6d998d7b4cd25f2ad96cc5c215d8
Youwen Wu <youwenw@gmail.com>
2025-01-06 23:40:09 -0800

    auto-update: 0.14.3 -> 0.14.4

--- gitbutler.nix
+++ gitbutler.nix
@@ -8,13 +8,30 @@
   fetchzip,
   ...
 }:
-appimageTools.wrapType2 {
-  inherit version;
+let
   pname = "gitbutler";
 
-  src = fetchzip {
-    inherit url hash;
+  srcZipped = fetchzip {
+    inherit hash url;
   };
+
+  appimageContents = appimageTools.extract {
+    inherit pname version;
+    src = "${srcZipped}/GitButler_${version}_amd64.AppImage.tar.gz";
+  };
+in
+appimageTools.wrapType2 rec {
+  inherit pname version;
+  src = "${srcZipped}/GitButler_${version}_amd64.AppImage.tar.gz";
+
+  extraInstallCommands = ''
+    # install -Dm755 unpack/usr/bin/gitbutler-tauri $out/bin/gitbutler-tauri
+    # install -Dm755 unpack/usr/bin/gitbutler-git-setsid $out/bin/gitbutler-git-setsid
+    # install -Dm755 unpack/usr/bin/gitbutler-git-askpass $out/bin/gitbutler-git-askpass
+
+    # cp -r unpack/usr/share $out/share
+    ls -a appimageContents
+  '';
 
   meta = {
     description = "Git client for simultaneous branches on top of your existing workflow";
